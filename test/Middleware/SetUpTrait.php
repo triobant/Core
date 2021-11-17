474741c2b858fa6bdf14d8f62e9431418af5bf3b
Midah Pasche <midahannep@gmail.com>
2021-11-17 16:22:41 +0100

    AuthHordSessionTest almost complete

--- test/Middleware/SetUpTrait.php
+++ test/Middleware/SetUpTrait.php
@@ -9,6 +9,7 @@
  * @license  http://www.horde.org/licenses/lgpl21 LGPL 2.1
  * @package  Core
  */
+
 namespace Horde\Core\Test\Middleware;
 
 use Psr\Http\Server\RequestHandlerInterface;
@@ -25,9 +26,9 @@ use Horde\Http\StreamFactory;
 use Horde\Http\ResponseFactory;
 use Horde\Http\Server\RampageRequestHandler;
 
-use \Horde_Session;
-use \Horde_Registry;
-use \Horde_Exception;
+use Horde_Session;
+use Horde_Registry;
+use Horde_Exception;
 
 trait SetUpTrait
 {
@@ -41,7 +42,11 @@ trait SetUpTrait
 
         $this->defaultPayloadResponse = $this->responseFactory->createResponse(200);
         $this->defaultPayloadHandler = $this->createMock(RequestHandlerInterface::class);
-        $this->defaultPayloadHandler->method('handle')->willReturn($this->defaultPayloadResponse);
+        $this->recentlyHandledRequest = null;
+        $this->defaultPayloadHandler->method('handle')->willReturnCallback(function ($request) {
+            $this->recentlyHandledRequest = $request;
+            return $this->defaultPayloadResponse;
+        });
 
         $this->handler = new RampageRequestHandler(
             $this->responseFactory,
