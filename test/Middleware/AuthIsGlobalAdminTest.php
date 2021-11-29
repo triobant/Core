2d235ba8ee5a2e4124ba5e1a026c8a3b5f6e5be8
Brian Ott <bott@b1-systems.de>
2021-11-29 15:33:54 +0100

    Test for AuthIsGlobalAdmin was created; it is similar to AuthHordeSession; currently unfinished

---
+++ test/Middleware/AuthIsGlobalAdminTest.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * Copyright 2016-2021 Horde LLC (http://www.horde.org/)
+ *
+ * See the enclosed file LICENSE for license information (LGPL). If you
+ * did not receive this file, see http://www.horde.org/licenses/lgpl21.
+ *
+ * @category Horde
+ * @license  http://www.horde.org/licenses/lgpl21 LGPL 2.1
+ * @package  Core
+ */
+
+ namespace Horde\Core\Test\Middleware;
+
+ use Horde\Core\Middleware\AuthIsGlobalAdmin;
+
+ use Horde\Test\TestCase;
+
+ use Horde_Session;
+ use Horde_Exception;
+ use Horde_Registry;
+
+ class AuthIsGlobalAdminTest extends TestCase
+ {
+     use SetUpTrait;
+
+     protected function getMiddleware()
+     {
+         return new AuthIsGlobalAdmin($this->registry);
+     }
+     
+     public function testIsAdmin()
+     {
+        $username = 'testuser01';
+        $middleware = $this->getMiddleware();
+        //$this->registry->method('isAuthenticated')->willReturn(true);
+        $this->registry->method('isAdmin')->willReturn(true); // will set true
+        $this->registry->method('getAuth')->willReturn($username);
+        $request = $this->requestFactory->createServerRequest('GET', '/test');
+        $response = $middleware->process($request, $this->handler);
+
+        //$authUser = $this->recentlyHandledRequest->getAttribute('HORDE_AUTHENTICATED_USER');
+        //$guestUser = $this->recentlyHandledRequest->getAttribute('HORDE_GUEST');
+        $authAdminUser = $this->recentlyHandledRequest->getAttribute('HORDE_GLOBAL_ADMIN');
+        // assert that $authAdminUser has the correct Value
+
+        $this->assertEquals($username, $authAdminUser);
+        //$this->assertNull($guestUser);
+
+        $this->assertEquals(200, $response->getStatusCode());
+     }
+
+     public function testIsNotAdmin()
+     {
+        $username = 'testuser01';
+        $middleware = $this->getMiddleware();
+        //$this->registry->method('isAuthenticated')->willReturn(true);
+        $this->registry->method('isAdmin')->willReturn(false); //will set false
+        $this->registry->method('getAuth')->willReturn($username);
+        $request = $this->requestFactory->createServerRequest('GET', '/test');
+        $response = $middleware->process($request, $this->handler);
+
+        //$authUser = $this->recentlyHandledRequest->getAttribute('HORDE_AUTHENTICATED_USER');
+        //$guestUser = $this->recentlyHandledRequest->getAttribute('HORDE_GUEST');
+        $authAdminUser = $this->recentlyHandledRequest->getAttribute('HORDE_GLOBAL_ADMIN');
+        // assert that $authAdminUser has the correct Value
+
+        $this->assertEquals(200, $response->getStatusCode());
+     }
+ }
